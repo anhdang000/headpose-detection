13d91c9eb1e46eb62287783aa31c1366e9c29073
anhdang000 <danglinh.anh000@gmail.com>
2021-08-14 15:36:01 +0700

    Fix duplicated cases

--- utils_funcs.py
+++ utils_funcs.py
@@ -7,20 +7,20 @@ def filter_records(record, patience=2):
     buff = []
     result = []
     for element in record:
-        if element != 'F':
-            buff = buff[1:] + [element]
-            if len(set(buff)) == 1 and len(buff) == patience:
-                if len(result) >= 1:
-                    if element != result[-1]:
-                        result.append(element)
-                else:
+        buff = buff[1:] + [element]
+        if len(set(buff)) == 1 and len(buff) == patience:
+            if len(result) >= 1:
+                if element != result[-1]:
                     result.append(element)
-            
+            else:
+                result.append(element)
+        
 
-            elif len(buff) < patience:
-                buff.append(element)
+        elif len(buff) < patience:
+            buff.append(element)
+    result = [x for x in result if x != 'F']
     return result
 
 if __name__ == "__main__":
-    record = ['F', 'F', 'R', 'R', 'R', 'R', 'L', 'L', 'L', 'U', 'D', 'D']
+    record = ['F', 'F', 'R', 'R', 'R', 'R', 'L', 'L', 'L', 'F', 'L', 'L', 'L', 'U', 'D', 'D']
     print(filter_records(record))
